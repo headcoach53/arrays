5051a78088a078d1a9c723a9dee13fcdcb67d039
headcoach53 <headcoach52@gmail.com>
2020-09-10 09:58:29 -0500

    Create array.py
    Something we did in class

---
+++ array.py
@@ -0,0 +1,10 @@
+artists = ["Linkin Park", "Queen", "Tyler The Creator", "ASAP Rocky", "Papa Roach"]
+print(artists)
+
+artists = ["Linkin Park", "Queen", "Tyler The Creator", "ASAP Rocky", "Papa Roach"]
+artists[4] = "Blink 182"
+print(artists)
+
+artists = ["Jay-Z", "Queen", "Bono", "G-Eazy", "Pink Floyd"]
+artists.append("Juicy J")
+print(artists)
